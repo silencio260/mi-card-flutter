9647f9e8596a3d4d364fa54bea25d154abfcfe7c
silencio260 <54043677+silencio260@users.noreply.github.com>
2020-07-29 13:02:15 +0100

    change image in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/faruq.jpg'),
+            )
+          ],
+        ),
       ),
-    ),
-  );
+    ));
+  }
 }
